e8296c1f2b8d75f7b6ef167283eb0061ab86622a
dracarys18 <me@karthihegde.dev>
2026-04-27 14:24:36 +0530

    template: mir

---
+++ crates/hades-mir/src/lib.rs
@@ -0,0 +1,15 @@
+mod mir;
+
+use hades_ast::TypedStmt;
+use hades_module::{ModuleSignatures, TypedModule};
+
+use mir::builder::MirBuilder;
+
+pub trait ToMir {
+    type Output;
+    fn to_mir(&self, builder: &mut MirBuilder) -> Self::Output;
+}
+
+pub fn lower(module: TypedModule, import_sigs: Vec<ModuleSignatures>) {
+    let mut dummy_builder = MirBuilder::new(hades_ast::Types::Void, hades_error::Span::default());
+}

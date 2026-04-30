d4207b7eb63c465578dfeb791b59a3f1da43d9ff
dracarys18 <me@karthihegde.dev>
2026-05-01 01:01:47 +0530

    wip

--- crates/hades-mir/src/mir/stmt.rs
+++ crates/hades-mir/src/mir/stmt.rs
@@ -1,15 +1,16 @@
 use hades_ast::{TypedStmt, Types};
-use hades_tokens::Op;
+use hades_tokens::{Name, Op};
 
-use crate::{BasicBlock, BlockAnd, BlockAndExt, ToMir, unpack};
 use crate::mir::builder::MirBuilder;
 use crate::mir::place::{Operand, Place};
+use crate::{BasicBlock, BlockAnd, BlockAndExt, ToMir, unpack};
 
 pub enum Rvalue {
     Use(Operand),
     BinaryOp(Op, Operand, Operand),
     UnaryOp(Op, Operand),
     Cast(Operand, Types),
+    Aggregate(Name, Vec<Operand>),
 }
 
 pub enum MirStmt {
@@ -19,16 +20,15 @@ pub enum MirStmt {
 impl ToMir for TypedStmt {
     type Output = ();
 
-    fn to_mir(&self, builder: &mut MirBuilder, block: BasicBlock) -> BlockAnd<()> {
+    fn to_mir(&self, builder: &mut MirBuilder, mut block: BasicBlock) -> BlockAnd<()> {
         match self {
             TypedStmt::Let(l) => l.to_mir(builder, block),
             TypedStmt::Return(r) => r.to_mir(builder, block),
             TypedStmt::TypedExpr(e) => {
-                let mut block = block;
                 unpack!(block = e.expr.to_mir(builder, block));
                 block.unit()
             }
-            _ => block.unit(),
+            _ => unimplemented!(),
         }
     }
 }
--- crates/hades-mir/src/mir/terminator.rs
+++ crates/hades-mir/src/mir/terminator.rs
@@ -1,3 +1,10 @@
+use crate::mir::place::{Operand, Place};
+
 pub(crate) enum Terminator {
     Return,
+    Call {
+        func: Operand,
+        args: Vec<Operand>,
+        dest: Place,
+    },
 }
